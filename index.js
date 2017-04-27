99ee1788dd45e7eec67bce2d4e876381343f9901
zhangsihao <spirit.line.523@gmail.com>
2017-04-27 12:12:43 +0800

    Add gentle closing

--- index.js
+++ index.js
@@ -16,40 +16,61 @@ const instance = new Instance({
     ip_prefix: process.env.DISPROXY_IP_PREFIX,
     internal_ip: process.env.DISPROXY_INTERNAL_IP,
     broadcast: process.env.DISPROXY_BROADCAST,
-    message_queue: process.env.DISPROXY_MESSAGE_QUEUE,
-    aliyun_region: process.env.DISPROXY_ALIYUN_REGION,
-    aliyun_access_key: process.env.DISPROXY_ALIYUN_ACCESS_KEY,
-    aliyun_access_secret: process.env.DISPROXY_ALIYUN_ACCESS_SECRET
+    message_queue: process.env.DISPROXY_MESSAGE_QUEUE
 });
 let opts = instance.getOptions();
-mongoose.connect(opts.mongo);
-const aliyun = new Aliyun(opts.aliyun_region, opts.aliyun_access_key, opts.aliyun_access_secret);
 
 const server = proxy(http.createServer());
 
+let ip;
+
+const mq = ConnectorManager.getMessageQueue(opts.message_queue);
+const broadcast = ConnectorManager.getBroadcast(opts.broadcast, (msg) => {
+    if (msg.type === 'PROXY_CLOSED' && msg.internal_ip === ip) {
+        console.log('Closing server...');
+        server.close(() => {
+            process.exit();
+        });
+    }
+});
+
+let closeCount = 0;
+
 process.on('SIGINT', () => {
     console.log('SIGINT received.');
-    server.close(() => {
-        process.exit();
-    });
+    if (++closeCount >= 2) {
+        server.close(() => {
+            process.exit();
+        });
+    } else {
+        broadcast.broadcast({
+            type: 'PROXY_OFFLINE_REQUEST',
+            internal_ip: ip
+        }).then(() => {
+            console.log('Requesting to offline this proxy node.');
+        });
+    }
 });
 
 (async () => {
     if (opts.internal_ip) {
-        let ip = opts.internal_ip;
+        ip = opts.internal_ip;
     } else {
-        let ip = await instance.getPrivateIp();
+        ip = await instance.getPrivateIp();
+    }
+    console.log('Your internal ip is', ip, 'or you can set it by environment variable DISPROXY_INTERNAL_IP.');
+    if (!ip) {
+        console.error('Cannot start a proxy node without internal ip.');
     }
     server.listen(3128, () => {
-        const mq = ConnectorManager.getMessageQueue(opts.broadcast, (msg) => {
-
-        });
         let port = server.address().port;
-        console.log('Your internal ip is', ip, 'or you can set it by environment variables.');
         console.log('Proxy server listening on port %d', port);
         mq.send({
             type: 'PROXY_ONLINE_REQUEST',
-            internal_ip: ip
-        });
+            internal_ip: ip,
+            port: port,
+            protocol: 'http',
+            node_type: 'ALIYUN'
+        }).then();
     });
 })();
